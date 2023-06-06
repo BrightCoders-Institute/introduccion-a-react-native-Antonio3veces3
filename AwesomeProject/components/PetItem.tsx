6f3ab5fc1f4523a736976c6ce5f395eadcb6da60
Antonio3veces3 <cramirez28@ucol.mx>
2023-06-05 23:59:21 -0600

    Add deleteButton to item Pet on the petList

--- AwesomeProject/components/PetItem.tsx
+++ AwesomeProject/components/PetItem.tsx
@@ -1,63 +1,82 @@
-import React from 'react'
-import { Image, StyleSheet, Text, View } from 'react-native'
-import { Pet } from '../Types/PetTypes'
+import React from 'react';
+import {Alert, Image, Pressable, StyleSheet, Text, View} from 'react-native';
+import {Pet} from '../Types/PetTypes';
 
-type props= {
-  pet: Pet,
-  key: number
-}
+type props = {
+  pet: Pet;
+  key: number;
+};
 
-function PetItem(props: props) : JSX.Element{
+function PetItem(props: props): JSX.Element {
+  const onClickDelete = () =>
+    Alert.alert('Alert Title', 'My Alert Msg', [
+      {
+        text: 'Cancel',
+        onPress: () => console.log('Cancel Pressed'),
+        style: 'cancel',
+      },
+      {text: 'OK', onPress: () => console.log('OK Pressed')},
+    ]);
+
   return (
     <View style={style.container}>
       <View style={style.containerImg}>
-      <Image
-        style={style.img}
-        source={require('../img/perro.jpg')}
-      />
+        <Image style={style.imgPet} source={require('../img/perro.jpg')} />
       </View>
-      <View style= {style.textContainer}>
+      <View style={style.textContainer}>
         <Text style={style.petName}>{props.pet.name}</Text>
         <Text style={style.birth}>Brith: {props.pet.birth}</Text>
         <Text style={style.owner}>Owner: {props.pet.owner}</Text>
       </View>
+      <View>
+        <Pressable onPress={onClickDelete}>
+          <Image
+            style={style.iconDelete}
+            source={require('../img/deleteIcon.png')}
+          />
+        </Pressable>
+      </View>
     </View>
-  )
+  );
 }
 
-const style= StyleSheet.create({
+const style = StyleSheet.create({
   container: {
     height: 100,
     marginTop: 15,
     borderRadius: 6,
     flexDirection: 'row',
-    backgroundColor: "#E7EBEF",
+    backgroundColor: '#E7EBEF',
     alignItems: 'center',
-    
   },
   containerImg: {
-    marginLeft: 20,
+    marginLeft: 10,
   },
-  img: {
+  imgPet: {
     width: 90,
     height: 90,
     borderRadius: 50,
   },
-  textContainer:{
+  textContainer: {
     marginLeft: 20,
   },
   petName: {
-    fontSize: 26,
+    fontSize: 25,
     fontWeight: 'bold',
     color: '#365940',
   },
-  birth: { 
-    fontSize: 16,
+  birth: {
+    fontSize: 14,
   },
   owner: {
     marginTop: 3,
-    fontSize: 16,
-  }
+    fontSize: 14,
+  },
+  iconDelete: {
+    height: 35,
+    width: 35,
+    marginLeft: 30,
+  },
 });
 
-export default PetItem
+export default PetItem;
